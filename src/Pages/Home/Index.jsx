a317a73f77be2437a7ec9a85a3bda4fca68e4c81
cristian1938 <cristian_1938@hotmail.com>
2024-03-09 20:09:05 -0500

    Se agrego el API de fakeStoreApi

--- src/Pages/Home/Index.jsx
+++ src/Pages/Home/Index.jsx
@@ -1,17 +1,34 @@
+import { useState, useEffect } from 'react'
 import Layout from "../../Components/Layout"
 import Card from "../../Components/Card"
+
 function Home() {
-  
+    const [items, setItem] = useState(null)
+
+    useEffect(() =>{
+      fetch('https://fakestoreapi.com/products')
+        .then(response => (response.json()))
+        .then(data => setItem(data))
+
+    },[]
+
+    )
 
     return (
       <Layout>
         Home
-        <Card/><Card/>
-        <Card/> 
+        <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
+        {
+          items?.map( item =>(
+            <Card key={item.id} data = {item}/>
+          ))
+        }
+
+        </div>
+        
         
       </Layout>
-       
-      
+ 
     )
   }
   
