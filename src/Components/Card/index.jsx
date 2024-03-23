eb407936a7ef071fb51b5da6ee70de1ee370bf38
cristian1938 <cristian_1938@hotmail.com>
2024-03-23 16:28:30 -0500

    Se agrego la orden de compra

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react'
+import { PlusIcon, CheckIcon } from '@heroicons/react/24/solid'
 import { ShoppingCartContext } from '../../Context'
 
 const Card = (data) => {
@@ -8,6 +9,7 @@ const Card = (data) => {
     context.openProductDetail()
     context.setProductToShow(productDetail)
   }
+  
   const addProductsToCart = (event, productData) => {
     event.stopPropagation()
     context.setCount(context.count + 1)
@@ -17,30 +19,39 @@ const Card = (data) => {
     console.log('CART: ', context.cartProducts)
   }
 
+  const renderIcon = (id) =>{
+    const isInCart = context.cartProducts.filter(product => product.id === id).length > 0
+    if(isInCart){
+      return(
+        <div
+          className='absolute top-0 right-0 flex justify-center items-center bg-black w-6 h-6 rounded-full m-2 p-1'>
+          <CheckIcon className='h-6 w-6 text-white'></CheckIcon>
+        </div>
+      )
 
-  // const addProductsToCart = (event, productData) =>{
-  //   event.stopPropagation()
-  //   context.setCount(context.count + 1)
-  //   context.setCartProducts({...context.cartProducts, productData})
-  //   context.openCheckoutSideMenu()
-  //   context.closeProductDetail()
-  //   console.log('CART', context.cartProducts)
+    }else{
+      return(
+        <div onClick={(event) => addProductsToCart(event, data.data)}      className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1 hover:bg-yellow-500'
+            >
+              {/* BOTON DE + */}
+              <svg  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+              </svg>
+        </div>
 
-  // }
+      )
 
+    }
+  }
+
+  
   return (
     <div className='bg-white cursor-pointer w-56 h-60 rounded-lg'
         onClick={() => showProduct(data.data)}>
         <figure className='relative mb-2 w-full h-4/5'>
           <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>{data.data.category}</span>
           <img className='w-full h-full object-cover rounded-lg' src={data.data.image} alt={data.data.title} />
-          <div onClick={(event) => addProductsToCart(event, data.data)}      className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1 hover:bg-red-500'
-          >
-            {/* BOTON DE + */}
-            <svg  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-            </svg>
-          </div>
+          {renderIcon(data.data.id)}
         </figure>
         <p className='flex justify-between'>
           <span className='text-sm font-light truncate'>{data.data.description}</span>
@@ -48,6 +59,6 @@ const Card = (data) => {
         </p>
       </div>
     )
-  }
   
+}
   export default Card
--- src/Components/OrderCard/index.jsx
+++ src/Components/OrderCard/index.jsx
@@ -1,24 +1,25 @@
+import { XMarkIcon } from '@heroicons/react/24/solid'
 
 const OrderCard = props => {
-  const{title, imageUrl, price} = props
+  const { id, title, imageUrl, price, handleDelete } = props
 
   return (
-    <div className='flex justify-between items-center'>
-        <div className='flex items-center gap-2' >
-            <figure className="w-20 h-20">
-                <img className="w-full h-full rounded-lg object-cover" src={imageUrl} alt={title}/>
-            </figure>
-            <p className="text-sm font-light">{title}</p>
-        </div>
-        <div className='flex items-center gap-2'>
-            <p className="text-lg font-medium">{price}</p>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                    </svg>
-        </div>
+    <div className="flex justify-between items-center mb-3">
+      <div className='flex items-center gap-2'>
+        <figure className='w-20 h-20'>
+          <img className='w-full h-full rounded-lg object-cover' src={imageUrl} alt={title} />
+        </figure>
+        <p className='text-sm font-light'>{title}</p>
+      </div>
+      <div className='flex items-center gap-2'>
+        <p className='text-lg font-medium'>{price}</p>
+        <XMarkIcon onClick={() => handleDelete(id)} className='h-6 w-6 text-black cursor-pointer'></XMarkIcon>
+      </div>
     </div>
-)
+  )
 }
+
+
 
 export default OrderCard
 
